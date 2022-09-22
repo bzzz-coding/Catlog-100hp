30cc52d352f570571a80eb08ce5b673e76295475
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-09-21 23:38:19 -0700

    Edited controllers/home.js and feed.ejs to convert birthday to age on server

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 const Cat = require("../models/Cat")
+const functions = require('../controllers/functions')
 const moment = require('moment')
 
 module.exports = {
@@ -8,7 +9,8 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const cats = await Cat.find().sort({ needsHomeBy: "asc" }).lean();
-      res.render("feed.ejs", { cats: cats, moment: moment });
+      cats.forEach(cat => cat.age = functions.getAgeFromBirthday(cat.birthday))
+      res.render("feed.ejs", { cats, moment: moment });
     } catch (err) {
       console.log(err);
     }
