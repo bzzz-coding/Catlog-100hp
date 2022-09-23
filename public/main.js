152ac8e08949f4ef3d6d801a30c82e2eb7446116
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-09-23 13:16:40 -0700

    Added toggle for light and dark theme

---
+++ public/main.js
@@ -0,0 +1,15 @@
+const toggle = document.querySelector('#theme-toggle')
+const html = document.querySelector('.html')
+
+
+// console.log(html.getAttribute('lang'))
+// console.log(html.getAttribute('data-theme'))
+
+
+
+toggle.addEventListener('click', function() {
+  let dataTheme = html.getAttribute('data-theme')
+  console.log(`clicked! previous theme: dataTheme`);
+  (dataTheme == 'night') ? html.setAttribute('data-theme', 'bumblebee') : html.setAttribute('data-theme', 'night')
+  console.log(`theme changed to ${html.getAttribute('data-theme')}`)
+})
