ccae5e9bde4a655230a26c310be7ca06ce026e2e
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-09-27 23:25:59 -0700

    Added client-side js to toggle catlog form display

--- public/main.js
+++ public/main.js
@@ -11,14 +11,40 @@ toggle.addEventListener('click', function() {
 
 
 // If 'urgent' is selected, toggle .hide class for needsHomeby input
-const urgent = document.querySelector('#urgent')
+const urgent = document.querySelector('#urgent') 
+
+if (urgent) {
+  urgent.addEventListener('change', function () {
+    const datePicker = document.querySelector('.need-home-by')
+    if (urgent.value == 'Yes') {
+      datePicker.classList.remove("no-display")
+    } else {
+      datePicker.classList.add('no-display')
+    }
+  });
+}
 
 
-urgent.addEventListener('change', function () {
-  const datePicker = document.querySelector('.need-home-by')
-  if (urgent.value == 'Yes') {
-    datePicker.classList.remove("no-display")
-  } else {
-    datePicker.classList.add('no-display')
-  }
-})
+
+// Click on Add New Catlog button to see form
+const catlogBtn = document.querySelector('#add-catlog')
+
+if (catlogBtn) {
+  catlogBtn.addEventListener('click', () => {
+    console.log('clicked')
+    const catlogForm = document.querySelector('#catlog-form')
+    catlogForm.classList.remove('no-display')
+    catlogBtn.classList.add('no-display')
+  })
+}
+
+const cancelBtn = document.querySelector('#cancel-addlog')
+
+if (cancelBtn) {
+  cancelBtn.addEventListener('click', () => {
+    console.log('clicked')
+    const catlogForm = document.querySelector('#catlog-form')
+    catlogForm.classList.add('no-display')
+    catlogBtn.classList.remove('no-display')
+  })
+}
